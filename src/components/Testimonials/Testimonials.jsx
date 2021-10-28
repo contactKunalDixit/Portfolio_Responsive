2939479b0004ba1b04f8bb1ca686fe73746f4274
Kunal Dixit <contactkunaldixit@gmail.com>
2021-10-28 17:49:30 +0800

    Updated

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,12 +1,95 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './testimonials.scss';
 
-const Testimonials = () => {
+import { ThemeContext } from '../../context';
+
+function Testimonials() {
+	const data = [
+		{
+			key: 1,
+			id: 1,
+			name: 'Tom Durden',
+			title: 'Senior Developer',
+			img: 'https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+			icon: 'assets/twitter.png',
+			desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.',
+		},
+		{
+			key: 2,
+			id: 2,
+			name: 'Alex Kalinski',
+			title: 'Co-Founder of DELKA',
+			img: 'https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+			icon: 'assets/youtube.png',
+			desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ',
+			featured: true,
+		},
+		{
+			key: 3,
+			id: 3,
+			name: 'Martin Harold',
+			title: 'CEO of ALBI',
+			img: 'https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+			icon: 'assets/linkedin.png',
+			desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem',
+		},
+		{
+			key: 1,
+			id: 1,
+			name: 'Tom Durden',
+			title: 'Senior Developer',
+			img: 'https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+			icon: 'assets/twitter.png',
+			desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.',
+		},
+		{
+			key: 2,
+			id: 2,
+			name: 'Alex Kalinski',
+			title: 'Co-Founder of DELKA',
+			img: 'https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+			icon: 'assets/youtube.png',
+			desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ',
+			featured: true,
+		},
+	];
+
+	const theme = useContext(ThemeContext);
+	const darkMode = theme.state.darkMode;
 	return (
 		<div className='testimonials' id='testimonials'>
-			<div className='testimonials-Wrapper'>Hello World</div>
+			<div
+				className='contact-bg'
+				style={{ backgroundColor: darkMode ? '#ffd6a539' : '#ffd6a5' }}
+			></div>
+			<div className='testimonials-wrapper'>
+				<h1 className='testimonials-Title'>Testimonials</h1>
+				<div className='testimonials-container'>
+					{data.map((i) => {
+						return (
+							<div
+								className={i.featured ? 'card featured' : 'card'}
+								key={i.key}
+							>
+								<div className='top'>
+									<img src='assets/right-arrow.png' className='left' alt='' />
+									<img className='user' src={i.img} alt='' />
+									<img className='right' src={i.icon} alt='' />
+								</div>
+								<div className='center'>
+									<p>{i.desc}</p>
+								</div>
+								<div className='bottom'>
+									<h3 className='bottom-userName'>{i.name}</h3>
+									<h4 className='bottom-userTitle'>{i.title} </h4>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
 		</div>
 	);
-};
+}
 
 export default Testimonials;
