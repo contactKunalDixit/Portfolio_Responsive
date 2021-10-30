c8a7664be095086d074b808a8162800210e20862
Kunal Dixit <contactkunaldixit@gmail.com>
2021-10-30 19:34:11 +0800

    Updated

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,78 +1,11 @@
 import React, { useContext } from 'react';
 import './testimonials.scss';
 import { ThemeContext } from '../../context';
+import { linkedinData } from '../../data';
 
-const Testimonials = () => {
+const Testimonials = (props) => {
 	const theme = useContext(ThemeContext);
 	const darkMode = theme.state.darkMode;
-
-	const data = [
-		{
-			key: 1,
-			id: 1,
-			name: 'Tom Durden',
-			title: 'Senior Developer',
-			img: 'https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-			icon: 'assets/twitter.png',
-			desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.',
-		},
-		{
-			key: 2,
-			id: 2,
-			name: 'Alex Kalinski',
-			title: 'Co-Founder of DELKA',
-			img: 'https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-			icon: 'assets/youtube.png',
-			desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ',
-		},
-		{
-			key: 3,
-			id: 3,
-			name: 'Martin Harold',
-			title: 'CEO of ALBI',
-			img: 'https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-			icon: 'assets/linkedin.png',
-			desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem',
-		},
-		{
-			key: 4,
-			id: 4,
-			name: 'Tom Durden',
-			title: 'Senior Developer',
-			img: 'https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-			icon: 'assets/twitter.png',
-			desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.',
-		},
-		{
-			key: 5,
-			id: 5,
-			name: 'Alex Kalinski',
-			title: 'Co-Founder of DELKA',
-			img: 'https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-			icon: 'assets/youtube.png',
-			desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ',
-		},
-		{
-			key: 6,
-			id: 6,
-			name: 'Alex Kalinski',
-			title: 'Co-Founder of DELKA',
-			img: 'https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-			icon: 'assets/youtube.png',
-			desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ',
-			featured: true,
-		},
-		{
-			key: 7,
-			id: 7,
-			name: 'Alex Kalinski',
-			title: 'Co-Founder of DELKA',
-			img: 'https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-			icon: 'assets/youtube.png',
-			desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ',
-			featured: true,
-		},
-	];
 
 	return (
 		<div
@@ -92,11 +25,11 @@ const Testimonials = () => {
 			<div className='testimonials-wrapper'>
 				<h1 className='testimonials-Title'>Testimonials</h1>
 				<div className='testimonials-container'>
-					{data.map((i) => {
+					{linkedinData.map((item) => {
 						return (
 							<div
 								className='card'
-								key={i.key}
+								key={item.key}
 								style={{
 									backgroundColor: darkMode ? '#2e1daa50' : '#407a1d39',
 									color: darkMode && '#ccebff',
@@ -104,8 +37,8 @@ const Testimonials = () => {
 							>
 								<div className='top'>
 									<img src='assets/right-arrow.png' className='left' alt='' />
-									<img className='user' src={i.img} alt='' />
-									<img className='right' src={i.icon} alt='' />
+									<img className='user' src={item.img} alt='' />
+									<img className='right' src={item.icon} alt='' />
 								</div>
 								<div
 									className='center'
@@ -114,17 +47,17 @@ const Testimonials = () => {
 										color: darkMode && '#ccebff',
 									}}
 								>
-									<p>{i.desc}</p>
+									<p>{item.desc}</p>
 								</div>
 								<div className='bottom'>
-									<h3 className='bottom-userName'>{i.name}</h3>
+									<h3 className='bottom-userName'>{props.name}</h3>
 									<h4
 										className='bottom-userTitle'
 										style={{
 											color: !darkMode && 'black',
 										}}
 									>
-										{i.title}{' '}
+										{item.title}{' '}
 									</h4>
 								</div>
 							</div>
