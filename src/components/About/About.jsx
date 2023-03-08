5ebd55109f99992602db0fbd9fbfcf161a4649f2
Kunal Dixit <contactkunaldixit@gmail.com>
2023-03-08 23:30:02 +0800

    Updated: About Page - 370px and beyond done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -20,86 +20,45 @@ const About = () => {
 					<div className='about-left'>
 						<div className='about-card'>
 							<div className='selfDesc'>
-								<div className='highlightsList'>
+								<div className='highlightsList largeScreen'>
 									<p>
-										As the world becomes increasingly digital, having a strong
-										online presence has never been more critical for businesses
-										and individuals alike. Whether you're trying to establish
-										yourself as an industry leader, promote a new product, or
-										simply connect with your audience, having an effective web
-										presence is essential. And that's where I come in.
+										In today's digital age, a strong online presence is more
+										critical than ever before. Whether you're an individual or a
+										business looking to establish yourself as an industry
+										leader, promote a new product, or connect with your
+										audience, having an effective web presence is essential.
+										That's where I come in.
 									</p>
-									<br />
+
 									<p>
-										With my expertise in digital marketing and web development,
-										I help companies and individuals create an online presence
-										that resonates with their target audience. From designing a
-										beautiful and user-friendly website to developing a
-										comprehensive digital marketing strategy, I provide the
-										tools and guidance needed to reach the right people in the
-										best way possible.
+										As an expert in digital marketing and web development, I
+										specialize in creating online presences that resonate with
+										target audiences. Whether you're a small startup or a large
+										corporation, I provide the tools and guidance needed to
+										reach the right people in the best possible way.
 									</p>
-									<br />
+
 									<p>
-										Whether you're a small startup or a large corporation, I
-										understand that every client has unique goals and needs.
-										That's why I work closely with my clients to understand
-										their vision and tailor my approach accordingly.
+										I understand that each client has unique goals and needs, so
+										I work closely with them to tailor my approach and create a
+										web presence that looks great and drives results.
 									</p>
-									<br />
+
 									<p>
-										By combining my technical expertise with a deep
-										understanding of your audience, I can create a web presence
-										that not only looks great but also drives results.
-									</p>
-									<br />
-									<p>
-										So if you're looking to establish or enhance your online
-										presence, let me help you get there. Together, we can create
-										a digital strategy that sets you apart from the competition
-										and connects you with the people who matter most.
+										Let me help you enhance your online presence, and together,
+										we can create a digital strategy that sets you apart from
+										the competition and connects you with the people who matter
+										most.
 									</p>
 								</div>
-								{/* <div className='desc_Summary'>
-									<div className='a-award'>
-										<img src={Award} alt='' className='a-award-img' />
-										<div className='a-award-texts'>
-											<h4
-												className='a-award-title'
-												style={{ color: darkMode ? '#fdffb6' : '#ebe5e5' }}
-											>
-												Brandon Hall Awards 2021
-											</h4>
-											<p className='a-award-desc'>
-												<span className='category-desc'>Category</span> : Best
-												Blended Learning
-											</p>
-											<p className='a-award-desc'>
-												<span className='category-desc'>Project</span>: Axis
-												Bank
-											</p>
-										</div>
-									</div>
-									<div className='a-award'>
-										<img src={Award} alt='' className='a-award-img' />
-										<div className='a-award-texts'>
-											<h4
-												className='a-award-title'
-												style={{ color: darkMode ? '#fdffb6' : '#ebe5e5' }}
-											>
-												Most Impactful Facilitator 2022
-											</h4>
-											<p className='a-award-desc'>
-												<span className='category-desc'>Category</span>:
-												Operational Excellence
-											</p>
-											<p className='a-award-desc'>
-												<span className='category-desc'>Project</span>: IHS
-												Markit
-											</p>
-										</div>
-									</div>
-								</div> */}
+								<div className='highlightsList smallScreen'>
+									<p>
+										I can enhance your online presence by creating effective
+										digital strategies tailored to your unique goals, connecting
+										you with your audience and setting you apart from the
+										competition
+									</p>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -109,7 +68,6 @@ const About = () => {
 								<h1 className='pageTitle'>What I Do</h1>
 							</div>
 
-							{/* <Testimonials></Testimonials> */}
 							<ProductList></ProductList>
 						</div>
 					</div>
